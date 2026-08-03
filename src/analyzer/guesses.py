642809b00a02a016011bb7a96b897f2d4295c55f
Davi Silva <doasilva2009@hotmail.com>
2026-08-03 18:24:53 -0300

    feat: implementing guessers for entropy

---
+++ src/analyzer/guesses.py
@@ -0,0 +1,12 @@
+from difflib import SequenceMatcher
+
+def guess_year(password: str) -> int:
+    c = 0
+    
+    for i in [str(i) for i in list(range(100, 10000))]:
+        if i in password or password in i:
+            c += 1
+            
+    return c
+
+print(guess_year("1234. 152125.   15214124. 1231 23 12"))
