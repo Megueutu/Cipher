a949daaf924a18f2fd059c76a5576fca471146a9
Davi Silva <doasilva2009@hotmail.com>
2026-08-04 18:45:21 -0300

    feat: implementing typing in variables

--- data/registry.py
+++ data/registry.py
@@ -1,39 +1,42 @@
 import json
+from typing import Any
 from pathlib import Path
+
 from src.domain.dataset import Dataset, BaseType, Category, FileFormat
 
-DATA_PATH = Path("data/analysis")
+DATA_PATH     = Path("data/analysis")
 REGISTRY_PATH = Path("data/registry.json")
 
 def _infer_basetype(filename: str) -> BaseType:
     return BaseType.EXAMPLE if ".example." in filename else BaseType.ORIGINAL
 
-def load_registry() -> dict:
+def load_registry() -> dict[str, Any]:
     with open(REGISTRY_PATH, "r", encoding="utf-8") as file:
         return json.load(file)
 
 def get_datasets(base_category: Category = None, basetype: BaseType = None) -> list[Dataset]:
-    registry, datasets = load_registry(), []
+    registry: dict = load_registry()
+    datasets: list[Dataset] = list()
 
     for str_category, files in registry["datasets"].items():
-        category = Category(str_category)
+        category: Category = Category(str_category)
 
         if not category == base_category and base_category:
             continue
 
         for item in files:
-            inferred_basetype = _infer_basetype(item["file"])
+            inferred_basetype: BaseType = _infer_basetype(item["file"])
 
             if basetype is not None and inferred_basetype != basetype:
                 continue
 
-            dataset = Dataset(item["file"], category, inferred_basetype, FileFormat(item["format"]))
+            dataset: Dataset = Dataset(item["file"], category, inferred_basetype, FileFormat(item["format"]))
             datasets.append(dataset)
 
     return datasets
 
 def resolve_path(dataset: Dataset) -> Path:
-    path = (DATA_PATH / dataset.category.value / dataset.filename)
+    path: Path = (DATA_PATH / dataset.category.value / dataset.filename)
 
     if not path.exists():
         raise FileNotFoundError(f"Dataset not found: {path}")
