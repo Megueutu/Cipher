14035a9e35ed46aeb11c84c3d61686b34cdb8ace
meguito <doasilva2009@hotmail.com>
2026-08-04 22:25:52 -0300

    feat: implementing guess by year with procedural reduce

--- src/analyzer/guesses.py
+++ src/analyzer/guesses.py
@@ -1,12 +1,29 @@
-from difflib import SequenceMatcher
+import re
+from datetime import date
+
+def _calculate_closeness(year: int) -> float:
+    cur: int = date.today().year
+    dis: int = abs(cur - year)
+
+    pnlty: float = 0.1
+    floor: float = 2000
+    value: float = floor
+
+    for _ in range(dis):
+        value -= pnlty
+        pnlty += 0.05
+
+    ceil: int = 30
+
+    return round(max(0, round(value, 4)) * ceil / floor, 2)
 
 def guess_year(password: str) -> int:
-    c = 0
-    
-    for i in [str(i) for i in list(range(100, 10000))]:
-        if i in password or password in i:
-            c += 1
-            
-    return c
+    mul: float = 0
+    c:   int   = 0
 
-print(guess_year("password"))
+    for n in re.findall(r"\d+", password):
+        if len(n) == 4:
+            mul = _calculate_closeness(int(n))
+            c += 1 * mul
+
+    return c
