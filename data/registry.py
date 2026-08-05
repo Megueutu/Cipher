4fa7acb53c622b5d63e37d535b29659beb7b5394
daaavi4 <davi.alves@picpay.com>
2026-08-05 17:18:24 -0300

    feat: implementing overcharge for registry loader

--- data/registry.py
+++ data/registry.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any
+from typing import Union
 from pathlib import Path
 
 from src.domain.dataset import Dataset, BaseType, Category, FileFormat
@@ -10,19 +10,34 @@ REGISTRY_PATH = Path("data/registry.json")
 def _infer_basetype(filename: str) -> BaseType:
     return BaseType.EXAMPLE if ".example." in filename else BaseType.ORIGINAL
 
-def load_registry() -> dict[str, Any]:
+def _load_registry() -> dict[str, list[dict[str, str]]]:
     with open(REGISTRY_PATH, "r", encoding="utf-8") as file:
         return json.load(file)
 
-def get_datasets(base_category: Category = None, basetype: BaseType = None) -> list[Dataset]:
-    registry: dict = load_registry()
+def _available_category(registry: dict[str, dict[str, list[dict[str, str]]]]) -> list[Category]:
+    return [Category(i) for i in list(registry["datasets"].keys())]
+
+def get_datasets(base_category: Union[Category, list[Category]] = None, basetype: BaseType = None) -> list[Dataset]:
+    registry: dict[str, dict[str, list[dict[str, str]]]] = _load_registry()
     datasets: list[Dataset] = list()
-
-    for str_category, files in registry["datasets"].items():
-        category: Category = Category(str_category)
-
-        if not category == base_category and base_category:
-            continue
+    category: Category
+    iterable: list[Category]
+    
+    if type(base_category) is not list and type(base_category) is Category:
+        iterable = [base_category]
+    
+    elif type(base_category) is list:
+        iterable = base_category
+    
+    elif isinstance(base_category, type(None)):
+        iterable = _available_category(registry=registry)
+    
+    else:
+        raise TypeError(f"Non available type for base_category: {type(base_category)}")
+        
+    for i, files in iterable:
+        if type(i) is not type(Category): category = Category(i)
+        else: category = i
 
         for item in files:
             inferred_basetype: BaseType = _infer_basetype(item["file"])
